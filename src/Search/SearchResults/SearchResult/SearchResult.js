11a8b89c4c817ea5092dddeb452c85136a75965b
Catherine Martin <cathysue86@gmail.com>
2021-03-12 18:13:24 -0800

    added ratings component

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './SearchResult.module.css';
+import {BusinessRating} from '../../../BusinessRating/BusinessRating';
 
 export function SearchResult() {
     
@@ -8,10 +9,10 @@ export function SearchResult() {
             <img src='https://via.placeholder.com/210' alt='business' className={styles['business-image']}/>
             <div className={styles['business-info']}>
                 <h2 className="subtitle">Brooklyn Boulders</h2>
-                <p>Rating</p>
+                <BusinessRating/>
                 <p>$$ <span className="tag">Rock Climbing</span> <span class="tag">Climbing</span></p>
             </div>
-            <div>
+            <div className={styles['contact-info']}> 
                 <p>(347) 834-9066</p>
                 <p>Example Street</p>
                 <p>Zipcode Brooklyn</p>
