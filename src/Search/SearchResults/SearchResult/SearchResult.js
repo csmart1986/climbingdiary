c82c6ed08b02999915d76028cd77c8fa1ff59817
Catherine Martin <cathysue86@gmail.com>
2021-03-12 17:45:29 -0800

    added business and address info, and edited img in search result component

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -5,9 +5,17 @@ export function SearchResult() {
     
     return (
         <div className={styles['search-result']}>
-            <img src='https://via.placeholder.com/150' alt='business'/>
-            <p>General Information</p>
-            <p>Address Information</p>
+            <img src='https://via.placeholder.com/210' alt='business' className={styles['business-image']}/>
+            <div className={styles['business-info']}>
+                <h2 className="subtitle">Brooklyn Boulders</h2>
+                <p>Rating</p>
+                <p>$$ <span className="tag">Rock Climbing</span> <span class="tag">Climbing</span></p>
+            </div>
+            <div>
+                <p>(347) 834-9066</p>
+                <p>Example Street</p>
+                <p>Zipcode Brooklyn</p>
+            </div>
         </div>
     );
 }
