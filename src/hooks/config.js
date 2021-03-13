6d4d62e39a0158b3144d818ce85b68112faf1ec0
Catherine Martin <cathysue86@gmail.com>
2021-03-13 09:41:34 -0800

    created config.js, started get request with fetch api

---
+++ src/hooks/config.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'https://api.yelp.com/v3';
+
+// REMOVE THIS TOKEN 
+// if token is saved in FE, this application is not production ready!  
+export const BEARER_TOKEN = '';
---
+++ src/hooks/yelp-api/api.js
@@ -0,0 +1,13 @@
+ import {API_BASE_URL, BEARER_TOKEN} from './config';
+ import queryString from 'query-string';
+
+ export function get(path, queryParams) {
+    const query = queryString.stringify(queryString);
+    return fetch(`${API_BASE_URL}${path}?${query}`, {
+        headers: {
+            Authorization: `Bearer ${BEARER_TOKEN}`,
+            Origin: 'localhost', 
+            withCredentials:
+        }
+    })
+ }
