733b50b8b76452d95054451fb0b9813136b69abe
Catherine Martin <cathysue86@gmail.com>
2021-03-12 20:54:36 -0800

    added useState hook to searchbar

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './SearchBar.module.css';
 
 export function SearchBar(props) {
+    const [term, setTerm] = useState(props.term || '');
+    const [location, setLocation] = useState(props.location || '');
+
+    function submit(evt) {
+        console.log('TERM: ', term);
+        console.log('LOCATION: ', location);
+        evt.preventDefault();
+    }
+
     // if props.small is NOT set, add an is-medium class
     const sizeClass = props.small ? '' : 'is-medium'
     return (
-        <div>
+        <form onSubmit={submit}>
             <div className="field has-addons">
                 <p className="control">
                     <button className={`button is-static ${sizeClass}`}>Search</button>
                 </p>
                 <p className="control">
-                    <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="climbing gym name"/>
+                    <input className={`input ${sizeClass} ${styles['input-control']}`} 
+                        onChange={(evt)  =>  setTerm(evt.target.value)}
+                        type="text" 
+                        placeholder="climbing gym name"/>
                 </p>
+                <div className="control">
+                    <div className={`button is-static ${sizeClass}`}>Near</div>
+                </div>
                 <p className="control">
-                    <button className={`button is-static ${sizeClass}`}>Near</button>
+                    <input className={`input ${sizeClass} ${styles['input-control']}`} 
+                        onChange={(evt)  =>  setLocation(evt.target.value)}
+                        type="text" 
+                        placeholder="Where"/>
                 </p>
-                <p className="control">
-                    <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="Where"/>
-                </p>
-                <div className={`button ${sizeClass} ${styles['search-button']}`}>
+                <div className={`button ${sizeClass} ${styles['search-button']}`} onClick={submit}>
                     <span className="icon is-small"><i className="fas fa-search"></i></span>
                 </div>
             </div>
-        </div>
+        </form>
     );
 }
