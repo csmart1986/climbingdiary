17642d6985b463ee2f564296d5850cfbc5ff8fdc
Catherine Martin <cathysue86@gmail.com>
2021-03-12 16:27:59 -0800

    edited search results summary

--- src/Search/SearchResultsSummary/SearchResultsSummary.js
+++ src/Search/SearchResultsSummary/SearchResultsSummary.js
@@ -8,6 +8,12 @@ export function SearchResultsSummary() {
                 <h1 className='subtitle'><strong>climbing gyms</strong> new york city</h1>
                 <p>Showing 1-10 out of 10 results</p>
             </div>
+            <div className={styles.filters}>
+                <button className='button'>
+                    <span className="icon"><i className="fas fa-clock"></i></span>
+                    <span>Open Now</span>
+                </button>
+            </div>
         </div>
     )
 }
