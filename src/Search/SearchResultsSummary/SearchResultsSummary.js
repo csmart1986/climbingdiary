0737712797ddb62853a166244e3af8442c467324
Catherine Martin <cathysue86@gmail.com>
2021-03-14 12:26:35 -0700

    added filter by dropdown to search results summary

--- src/Search/SearchResultsSummary/SearchResultsSummary.js
+++ src/Search/SearchResultsSummary/SearchResultsSummary.js
@@ -6,17 +6,23 @@ export function SearchResultsSummary(props) {
     if (props.amountResults && props.shownResults) {
         resultsStats = <p>Showing 1-{props.shownResults} out of {props.amountResults} results</p>
     }
+    
     return (
         <div className={styles.container}>
             <div className={styles['search-summary']}>
                 <h1 className='subtitle'><strong>{props.term}</strong> {props.location}</h1>
                 {resultsStats}
-            </div>
-            <div className={styles.filters}>
-                <button className='button'>
-                    <span className="icon"><i className="fas fa-clock"></i></span>
-                    <span>Open Now</span>
-                </button>
+                <div>
+                    <form>
+                        <label className={styles.filter}>Filter By: 
+                            <select  className={styles.select} >
+                                <option value='all'>All gyms</option>
+                                <option value='Been there'>Been there, climbed that!</option>
+                                <option value='Still need to visit'>Still need to visit!</option>
+                            </select>
+                        </label>
+                    </form>
+                </div>
             </div>
         </div>
     )
