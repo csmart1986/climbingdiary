0d2b3b1f90ef4fe6537f2111b50ead9c2bdca933
Catherine Martin <cathysue86@gmail.com>
2021-03-13 21:57:11 -0800

    made phone number, address dynamic in searchresult component

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -9,18 +9,19 @@ export function SearchResult(props) {
         return <div></div>
     }
 
+    const addressLines = business.location.display_address.map(addressLine => <p key={business.id + addressLine}>{addressLine}</p>);
+
     return (
         <div className={styles['search-result']}>
             <img src={business.image_url} alt='business' className={styles['business-image']}/>
             <div className={styles['business-info']}>
                 <h2 className="subtitle">{business.name}</h2>
                 <BusinessRating reviewCount={business.reviewCount} rating={business.rating}/>
-                <p>$$ <span className="tag">Rock Climbing</span> <span className="tag">Climbing</span></p>
+                <p><span className="tag">Rock Climbing</span> <span className="tag">Climbing</span></p>
             </div>
             <div className={styles['contact-info']}> 
-                <p>(347) 834-9066</p>
-                <p>Example Street</p>
-                <p>Zipcode Brooklyn</p>
+                <p>{business.phone}</p>
+                {addressLines}
             </div>
         </div>
     );
