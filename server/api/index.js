73b0337225fb49ccb890aa841f6003f750e90fb6
Catherine Martin <cathysue86@gmail.com>
2021-04-22 14:19:05 -0700

    adds additional API routes to server/api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 
+// api/users
+router.use('/users', require('./users'));
+// api/notes
+router.use('/notes', require('./notes'));
+// api/gyms
+router.use('/gyms', require('./gyms'));
 
-//router.use('/users', require('./users'));
-// FIX ME - SET SUPER BROAD
-router.use('*', require('./notes'));
-
+// handle 404 error
 router.use((req, res, next) => {
   const error = new Error('Not Found on API server route');
   error.status = 404;
