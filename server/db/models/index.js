36d1ea98c026cfa2b4b902480c519bdd15420ed5
Catherine Martin <cathysue86@gmail.com>
2021-04-22 14:51:07 -0700

    BE: adds more associations to models/index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,18 @@
 const User = require('./user');
 const Note = require('./note');
+const Gym = require('/gym');
 
 // ASSOCIATIONS
 Note.belongsTo(User);
 User.hasMany(Note);
 
+User.hasMany(Gym);
+
+Note.belongsTo(Gym);
+Gym.hasMany(Note);
+
 module.exports = {
   User,
-  Note
+  Note, 
+  Gym
 }
