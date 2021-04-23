c4e5d8b4e670118ce670fc7e5ae0c0dfa4930571
Catherine Martin <cathysue86@gmail.com>
2021-04-22 20:08:40 -0700

    deletes extra files

--- src/hooks/yelp-api/api.js
+++ src/hooks/yelp-api/api.js
@@ -1,13 +1,14 @@
- import {API_BASE_URL, BEARER_TOKEN} from './config';
- import queryString from 'query-string';
+import {API_BASE_URL} from './config';
+import queryString from 'query-string';
+import YELP_API from './secrets';
 
- export function get(path, queryParams) {
-    const query = queryString.stringify(queryParams)
-    return fetch(`${API_BASE_URL}${path}?${query}`, {
-        headers: {
-            Authorization: `Bearer ${BEARER_TOKEN}`,
-            Origin: 'localhost', 
-            withCredentials: true
-        }
-    })
- }
+export function get(path, queryParams) {
+const query = queryString.stringify(queryParams)
+return fetch(`${API_BASE_URL}${path}?${query}`, {
+    headers: {
+        Authorization: `Bearer ${YELP_API}`,
+        Origin: 'localhost', 
+        withCredentials: true
+    }
+})
+}
