d637aab9abfa00798f2794c28186187cdb654b10
Catherine Martin <cathysue86@gmail.com>
2021-04-28 18:42:18 -0700

    FE: removes an import in HomePage

--- public/bundle.js
+++ public/bundle.js
@@ -1994,8 +1994,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "HomePage": () => (/* binding */ HomePage)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
-
 
 var HomePage = function HomePage() {
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
