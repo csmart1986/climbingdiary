152d1e617c4dde3b58c2b2b960df40be13cf1e22
Catherine Martin <cathysue86@gmail.com>
2021-07-11 17:26:19 -0700

    BE: adds more fields to User model

--- server/db/User.js
+++ server/db/User.js
@@ -49,6 +49,13 @@ const User = db.define('user', {
       return () => this.getDataValue('salt');
     },
   },
+  googleId: {
+    type: Sequelize.STRING
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
 });
 
 /**
